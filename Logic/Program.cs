fc252f08da750f8f3af69e94f21a141edfd22ab7
Christopher Odoala <odoala21@gmail.com>
2019-10-08 12:38:49 +0100

    Update Logic

--- Logic/Program.cs
+++ Logic/Program.cs
@@ -13,7 +13,17 @@ namespace Logic
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(Check.Marketer.ToString());
+            //Console.WriteLine(Check.Marketer.ToString());
+            //Console.ReadLine();
+
+            var newString = "rc645832";
+            //var newString2 = "BN6453822";
+            //var newString3 = "TR24263489";
+
+            if (!newString.StartsWith("RC") && !newString.StartsWith("BN"))
+                Console.WriteLine("Invalid");
+            else
+                Console.WriteLine("Its Correct");
             Console.ReadLine();
         }
     }
